456a532b802c48e9c0feeb6f2f0e51009c92cb89
Blake Gotting and Miles Bradt <miles@>
2018-10-17 10:03:51 -0700

    add letter content and will place user input, awaiting css styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     var userNameInput = $("input#userName").val();
+    $("#name").text(userNameInput);
+    $("#postcard").show(600);
     console.log(userNameInput);
     event.preventDefault();
   });
